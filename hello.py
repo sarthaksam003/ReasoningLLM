b7e285e50930ad4f7530d14100a249fd44c88b7c
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-16 09:58:29 +0530

    Added a seperate PARAM token

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ import os
 output_path = os.path.join("outputs", "testHelloOutput.txt")
 
 with open(output_path, "a") as f:
-    f.write(f"Pipeline test done successfully @ {datetime.now()}\n")
+    f.write(f"Pipeline test was done successfully @ {datetime.now()}\n")
