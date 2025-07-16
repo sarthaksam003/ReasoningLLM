10487c5cb8a71f103b7d7d59d0bfbe2a3b778ccf
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-16 11:36:24 +0530

    Updated YAML to use TOKEN

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ import os
 output_path = os.path.join("outputs", "testHelloOutput.txt")
 
 with open(output_path, "a") as f:
-    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} or 10:57 am at 16 July 2025 before going for tea break\n")
+    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} or 11:36 am at 16 July 2025 after coming from tea break\n")
