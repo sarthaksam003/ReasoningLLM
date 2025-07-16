8a1cd5a343234bb9fc2aa52a501e6a9c8d24a113
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-16 10:52:30 +0530

    Updated YAML

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ import os
 output_path = os.path.join("outputs", "testHelloOutput.txt")
 
 with open(output_path, "a") as f:
-    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} while listening to 'Daddy Cool' by Boney M\n")
+    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} or 10:52 am at 16 July 2025 while listening to 'Daddy Cool' by Boney M\n")
