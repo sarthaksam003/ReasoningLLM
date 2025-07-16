9621c55c24a76578da7ec47206ea5fc2d7b8d135
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-16 09:18:49 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 with open("helloTestOutput.txt", "a") as f:
-    f.write(f"Pipeline test completed at {datetime.now()}\n")
+    f.write(f"Pipeline test successful at {datetime.now()}\n")
