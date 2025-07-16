d40fbd579c0f31f78a06f98199c754d6229ce482
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-16 12:06:22 +0530

    Updated YAML to name output files dynamically and also push .err and .out files into the outputs folder

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import os
-output_path = os.path.join("outputs", "testHelloOutput.txt")
 
-with open(output_path, "a") as f:
-    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} or 11:52 am at 16 July 2025 after coming from tea break\n")
+# Get the dynamic filename from environment variable set by SLURM job
+# If not set, fallback to the old filename
+output_filename = os.environ.get('OUTPUT_FILENAME', 'testHelloOutput.txt')
+
+with open(output_filename, "a") as f:
+    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} or 12:05 pm at 16 July 2025 after coming from tea break\n")
