a09316383870982d9b131c3da8339f41994d2feb
sarthaksam003 <sarthaksam003@gmail.com>
2025-07-17 10:21:03 +0530

    Updated YAML code to run for multiple branches, renamed hello.py to main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import os
+
+# Get the dynamic filename from environment variable
+# This will be something like "output_20250716_121530.txt"
+output_filename = os.environ.get('OUTPUT_FILENAME', 'default_output.txt')
+
+with open(output_filename, "a") as f:
+    f.write(f"Pipeline test conducted at exactly @ {datetime.now()} from main.py\n")
